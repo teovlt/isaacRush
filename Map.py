a23401cff14a5c482138506f9a9690eb3cdc0209
felix <felix.picon@etu.univ-grenoble-alpes.fr>
2023-09-18 18:35:34 +0200

    Map.py amélioré changement des variables adapté au csv, remove de la résolution

--- Map.py
+++ Map.py
@@ -1,14 +1,15 @@
 import pygame as pg
 import Bloc
 
-matriceBlocs = [[0 for x in range(50)] for y in range(50)]
+
 
 # 1280*720
 class Map:
     """
     Classe Map
     """
-    def __init__(self, screen, cvsFile, resolution):
+
+    def __init__(self, screen, cvsFile):
         """
         Constructeur de la classe Map
         :param cvsFile: chemin du fichier csv
@@ -16,7 +17,8 @@ class Map:
         :param screen: écran sur lequel on dessine la map
         :return: None
         """
-        self.resolution = resolution
+
+
         self.screen = screen
         self.initFromCsv(cvsFile)
 
@@ -29,38 +31,42 @@ class Map:
         :return: None
         """
 
-        if matriceBlocs != None:
-            # lecture du fichier csv
-            nbLigne = 0
-            # Ouverture du fichier en lecture seule, en mode texte
-            file = open(cvsFile, "r")
-            # Récupération de la liste des lignes du fichier dans une liste
-            lignes = file.read().split("\n")
-            # supression de la dernière ligne vide
-            lignes.pop()
+        # TODO variable à mettre dans un fichier variable d'environnement
+        HAUTEUR_BLOC = 25
+        LARGEUR_BLOC = 25
 
-            # Initialisation de la matrice de blocs avec le bon nombre de lignes en fonction du csv
-            self.blocs = [[] for i in range(lignes.__len__())]
 
-            # On récupère chaque éléments des lignes et on les transforme en entier
-            for ligne in lignes:
-                line = ligne.split(",")
-                for i in range(len(line)):
-                    line[i] = int(line[i])
-                self.blocs[nbLigne] = line
-                nbLigne += 1
-            # fermeture du fichier
-            file.close()
+        # lecture du fichier csv
+        nbLigne = 0
+        # Ouverture du fichier en lecture seule, en mode texte
+        file = open(cvsFile, "r")
+        # Récupération de la liste des lignes du fichier dans une liste
+        lignes = file.read().split("\n")
+        # supression de la dernière ligne vide
+        lignes.pop()
 
-            # Parcours de la matrice pour créer les blocs chque 1 est remplaçé par un bloc
-            i = 0
-            while i < self.resolution[1]:
-                j = 0
-                while j < self.resolution[0]:
-                    if self.blocs[i][j] == 1:
-                        self.blocs[i][j] = Bloc.Bloc(i * 25, j * 25)
-                    j += 1
-                i += 1
+        # Initialisation de la matrice de blocs avec le bon nombre de lignes en fonction du csv
+        self.blocs = [[] for i in range(lignes.__len__())]
+
+        # On récupère chaque éléments des lignes et on les transforme en entier
+        for ligne in lignes:
+            line = ligne.split(",")
+            for i in range(len(line)):
+                line[i] = int(line[i])
+            self.blocs[nbLigne] = line
+            nbLigne += 1
+        # fermeture du fichier
+        file.close()
+
+        # Parcours de la matrice pour créer les blocs chaque 1 est remplaçé par un bloc
+        x = 0
+        while x < self.blocs[0].__len__():
+            j = 0
+            while j < self.blocs.__len__():
+                if self.blocs[j][x] == 1:
+                    self.blocs[j][x] = Bloc.Bloc(x * HAUTEUR_BLOC, j * LARGEUR_BLOC)
+                j += 1
+            x += 1
 
 
 
