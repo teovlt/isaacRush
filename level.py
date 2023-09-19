b4ee4c94772042a796aa3810ad2dcabd97893cc4
teovlt <teo.villet2@gmail.com>
2023-09-19 16:01:59 +0200

    add audio + time when game

--- level.py
+++ level.py
@@ -6,6 +6,7 @@ from settings import tileSize, screenWidth
 from npc import Npc
 from spike import Spike
 
+
 class Level:
     def __init__(self, surface, csv):
         self.csv = csv
@@ -13,16 +14,16 @@ class Level:
         self.setupLevel(csv)
         self.worldShift = 0
         self.currentX = 0
+        self.finish = False
 
     def setupLevel(self, csv):
         self.tiles = pygame.sprite.Group()
         self.player = pygame.sprite.GroupSingle()
         self.npcs = pygame.sprite.Group()
 
-
         file = open(csv, "r")
         rows = file.read().split("\n")
-        rows.pop() # Supprimer la dernière ligne vide
+        rows.pop()  # Supprimer la dernière ligne vide
 
         for rowIndex, row in enumerate(rows):
             row = row.split(",")
@@ -43,7 +44,6 @@ class Level:
                     npc = Npc((x * tileSize, y * tileSize + tileSize / 2))
                     self.npcs.add(npc)
 
-
     def horizontalMovementCollision(self):
         player = self.player.sprite
         player.rect.x += player.direction.x * player.speed
@@ -51,6 +51,7 @@ class Level:
         for sprite in self.tiles.sprites():
             if sprite.rect.colliderect(player.rect) and sprite.deadly:
                 self.setupLevel(self.csv)
+                self.finish = True
             elif sprite.rect.colliderect(player.rect):
                 if player.direction.x < 0:
                     player.rect.left = sprite.rect.right
@@ -58,7 +59,6 @@ class Level:
                 elif player.direction.x > 0:
                     player.rect.right = sprite.rect.left
                     self.current_x = player.rect.right
-
 
     def horizontalMovementCollisionNpcs(self):
         npcs = self.npcs.sprites()
@@ -73,8 +73,8 @@ class Level:
                     npc.rect.right = sprite.rect.left
                     npc.direction.x = -1
                 elif npc.rect.colliderect(player.rect):
-                     self.setupLevel(self.csv)
-
+                    self.setupLevel(self.csv)
+                    self.finish = True
 
     def verticalMovementCollision(self):
         player = self.player.sprite
@@ -83,6 +83,7 @@ class Level:
         for sprite in self.tiles.sprites():
             if sprite.rect.colliderect(player.rect) and sprite.deadly:
                 self.setupLevel(self.csv)
+                self.finish = True
             elif sprite.rect.colliderect(player.rect):
                 if player.direction.y > 0:
                     player.rect.bottom = sprite.rect.top
@@ -91,7 +92,6 @@ class Level:
                 elif player.direction.y < 0:
                     player.rect.top = sprite.rect.bottom
                     player.direction.y = 0
-
 
         if player.onGround and player.direction.y < 0 or player.direction.y > 1:
             player.onGround = False
@@ -116,7 +116,6 @@ class Level:
         self.tiles.update(self.worldShift)
         self.tiles.draw(self.displaySurface)
 
-
         # player
         self.player.update()
         self.horizontalMovementCollision()
@@ -124,7 +123,7 @@ class Level:
 
         self.scrollX()
         self.player.draw(self.displaySurface)
-                                                                                
+
         # npcs
         self.npcs.draw(self.displaySurface)
         self.npcs.update(self.worldShift)
--- main.py
+++ main.py
@@ -2,22 +2,50 @@
 import pygame, sys
 from level import Level
 from settings import *
+import pygame.mixer
 
 # Pygame setup
 pygame.init()
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 clock = pygame.time.Clock()
+
+#Temps
+startTime = 0
+
+
 level = Level(screen, "./map.csv")
 pygame.display.set_caption("NOM DU JEU")
+#son
+pygame.mixer.init()
+son = pygame.mixer.Sound("Audio/song.mp3")
+son.set_volume(0)
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            son.stop()
             pygame.quit()
             sys.exit()
 
     screen.fill("black")
+    if startTime is None or level.finish:
+        startTime = pygame.time.get_ticks()
+        level.finish = False
     level.run()
+    son.play()
+
+    font = pygame.font.Font(None, 36)
+    current_time = pygame.time.get_ticks() - startTime
+    current_time_str = f"Time: {current_time // 1000} s"
+    #best_time_str = f"Best Time: {bestTime // 1000} s"
+    current_time_text = font.render(current_time_str, True, "white")
+    #best_time_text = font.render(best_time_str, True, "white")
+    screen.blit(current_time_text, (10, 10))
+    #screen.blit(best_time_text, (10, 50))
+
 
     pygame.display.update()
     clock.tick(60) # limiter à 60fps
+
+
