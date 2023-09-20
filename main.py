05777d9ab8610ffde7f23dbf0b3cba1eb17119fa
Gabriel Halus <gabrielhalus@gmail.com>
2023-09-20 10:08:32 +0200

    Fix close functions

--- main.py
+++ main.py
@@ -37,16 +37,44 @@ def run():
 
     pygame.quit()
 
+def mainMenu():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                for button in menu.buttons:
+                    if button.rect.collidepoint(event.pos) and button.action:
+                        button.action()
+
+        # Effacer l'écran
+        screen.fill('black')
+
+        # Afficher le titre du jeu dans le menu
+        displayText(screen, "Menu Mystère", 100, screenWidth // 2, 150, 'white')
+
+        # Afficher le menu
+        menu.displayButtons(screen)
+
+        # Mettre à jour l'affichage
+        pygame.display.flip()
+
 def pause():
     paused = True
     while paused:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                quit()
+                sys.exit()
+
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     paused = False  # Mettre paused à False pour reprendre le jeu
+
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     for button in menuPause.buttons:
@@ -75,31 +103,6 @@ def resume():
 
 def quit():
     mainMenu()
-
-def mainMenu():
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                quit()
-
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                for button in menu.buttons:
-                    if button.rect.collidepoint(event.pos) and button.action:
-                        button.action()
-
-        # Effacer l'écran
-        screen.fill('black')
-
-        # Afficher le titre du jeu dans le menu
-        displayText(screen, "Menu Mystère", 100, screenWidth // 2, 150, 'white')
-
-        # Afficher le menu
-        menu.displayButtons(screen)
-
-        # Mettre à jour l'affichage
-        pygame.display.flip()
 
 # Créer le menu et les boutons
 menu = Menu()
