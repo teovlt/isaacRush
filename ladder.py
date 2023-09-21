fee51bf15dac6a26165ac2260300cc7eb58dc6ad
teovlt <teo.villet2@gmail.com>
2023-09-21 16:47:09 +0200

    fix bug ladder

--- ladder.py
+++ ladder.py
@@ -4,7 +4,7 @@ import pygame
 class Ladder(tile.Tile):
     def __init__(self, pos, size):
         super().__init__(pos, size)
-        self.image = pygame.image.load("Graphics/Ladders/2.png")
+        self.image = pygame.image.load("Graphics/Ladders/ladder.png")
         self.image = pygame.transform.scale(self.image, (size, size * 8))  # Redimensionner si nécessaire
         self.rect = self.image.get_rect(topleft=pos)
         self.ladder = True
